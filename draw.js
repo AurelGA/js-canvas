eba399ba77dc8365a66401ea09cc4d69df81520e
Steve Geluso <stevegeluso@gmail.com>
2017-02-20 15:25:48 -0800

    adding mouse interactions, clear button

--- draw.js
+++ draw.js
@@ -6,12 +6,25 @@ function main() {
   // 3D so we grab 2D.
   var canvas = document.getElementById("easel");
   var ctx = canvas.getContext("2d");
-
+  
   // It's possible for the canvas to be a different size than the element on the
   // page. If the canvas were smaller than the element than it would be stretched.
   // Let's use jQuery to measure the size of the element and set the canvas to that.
   canvas.width = $(canvas).width();
   canvas.height = $(canvas).height();
+  
+  // It turns out to be useful to attach the width and height properties to the
+  // ctx too, so it knows how big it's canvas is.
+  ctx.width = canvas.width;
+  ctx.height = canvas.height;
+
+  canvas.addEventListener("click", function(event) {
+    drawRectangle(ctx, event.offsetX, event.offsetY);
+  });
+  
+  document.getElementById("clear").addEventListener("click", function(event) {
+    clearCanvas(ctx);
+  });
 
   // Define some variables for the x,y coordinates of where we'll draw
   // and the width and height of rectangles we'll draw.
@@ -31,4 +44,20 @@ function main() {
   // then stroke around it's edges with red.
   ctx.fillRect(xx, yy, width, height);
   ctx.strokeRect(xx, yy, width, height);
+}
+
+function drawRectangle(ctx, x, y) {
+  // these rectangles will be 50x50px squares
+  var size = 50;
+  
+  ctx.fillStyle = 'purple';
+  ctx.stokeStyle = 'yellow';
+  ctx.lineWidth = 5;
+  
+  ctx.fillRect(x, y, size, size);
+  ctx.strokeRect(x, y, size, size);
+}
+
+function clearCanvas(ctx) {
+  ctx.clearRect(0, 0, ctx.width, ctx.height);
 }
